cf1fca1afc1b15cdabfe2a425f3bf9261df7e8cc
lilianaaa804 <liliana@kakao.com>
2021-04-21 11:31:38 +0900

    Part 6-24

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -2,6 +2,8 @@ package org.zerock.controller;
 
 import lombok.extern.log4j.Log4j;
 import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,12 +13,17 @@ import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.AttachFileDTO;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -154,5 +161,88 @@ public class UploadController {
             e.printStackTrace();
         }
         return result;
+    }
+
+    /* 크롬에서만 적용 가능
+    @GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public ResponseEntity<Resource> downloadFile(String fileName){
+        log.info("download file: " + fileName);
+        Resource resource = new FileSystemResource("/Users/yeonjinoh/Documents/upload/"+fileName);
+        log.info("resource: "+resource);
+
+        String resourceName = resource.getFilename();
+        HttpHeaders headers = new HttpHeaders();
+        try{
+            headers.add("Content-Disposition", "attachment; filename="
+                    + new String(resourceName.getBytes("UTF-8"), "ISO-8859-1"));
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+    }*/
+
+    @GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent")String userAgent, String fileName){
+        log.info("download file: " + fileName);
+        Resource resource = new FileSystemResource("/Users/yeonjinoh/Documents/upload/"+fileName);
+
+        if(resource.exists() == false){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        String resourceName = resource.getFilename();
+
+        //remove UUID
+        String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+
+        HttpHeaders headers = new HttpHeaders();
+        try{
+            String downloadName = null;
+            if(userAgent.contains("Trident")){
+                log.info("IE browser");
+                downloadName = URLEncoder.encode(resourceOriginalName,"UTF-8").replaceAll("\\+", " ");
+            }else if(userAgent.contains("Edge")){
+                log.info("Edge browser");
+                downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8");
+                log.info("Edge name: "+downloadName);
+            }else{
+                log.info("Chrome browser");
+                downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
+            }
+
+            headers.add("Content-Disposition", "attachment; filename=" + downloadName);
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+    }
+
+    @PostMapping("/deleteFile")
+    @ResponseBody
+    public ResponseEntity<String> deleteFile(String fileName, String type){
+        log.info("deleteFile: "+ fileName);
+
+        File file;
+
+        try{
+            file = new File("/Users/yeonjinoh/Documents/upload/" + URLDecoder.decode(fileName, "UTF-8"));
+
+            file.delete();
+
+            if(type.equals("image")){
+                String largeFileName = file.getAbsolutePath().replace("s_","");
+
+                log.info("largeFileName: "+largeFileName);
+
+                file = new File(largeFileName);
+                file.delete();
+            }
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<String>("deleted", HttpStatus.OK);
     }
 }
--- ex05/target/ex05-1.0-SNAPSHOT/WEB-INF/views/uploadAjax.jsp
+++ ex05/target/ex05-1.0-SNAPSHOT/WEB-INF/views/uploadAjax.jsp
@@ -33,9 +33,39 @@
     }
 
     .uploadResult ul li img {
-        width: 20px;
+        width: 100px;
+    }
+    .uploadResult ul li span{
+        color: white;
+    }
+    .bigPictureWrapper {
+        position: absolute;
+        display: none;
+        justify-content: center;
+        align-items: center;
+        top:0%;
+        width:100%;
+        height:100%;
+        background-color: gray;
+        z-index: 100;
+        background:rgba(255,255,255,0.5);
+    }
+
+    .bigPicture {
+        position: relative;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .bigPicture img{
+        width:600px;
     }
 </style>
+
+<div class="bigPictureWrapper">
+    <div class="bigPicture">
+    </div>
+</div>
 
 <div class="uploadDiv">
     <input type="file" name="uploadFile" multiple>
@@ -53,6 +83,37 @@
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
         crossorigin="anonymous"></script>
 <script>
+    function showImage(fileCallPath){
+        //alert(fileCallPath);
+        $(".bigPictureWrapper").css("display","flex").show();
+
+        $(".bigPicture")
+        .html("<img src='/display?fileName="+encodeURI(fileCallPath)+"'>")
+        .animate({width:'100%', height:'100%'},1000);
+    }
+    $(".bigPictureWrapper").on("click", function(e){
+        $(".bigPicture").animate({width:'0%', height: '0%'}, 1000);
+        setTimeout(()=>{
+            $(this).hide();
+        },1000);
+    });
+
+    $(".uploadResult").on("click","span", function (e){
+
+        var targetFile = $(this).data("file");
+        var type = $(this).data("type");
+        console.log(targetFile);
+        $.ajax({
+            url:'deleteFile',
+            data:{fileName: targetFile, type:type},
+            dataType:'text',
+            type:'POST',
+            success: function(result){
+                alert(result);
+            }
+        });
+    });
+
     $(document).ready(function (){
 
         var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
@@ -98,13 +159,25 @@
                 var str ="";
                 $(uploadResultArr).each(
                     function (i, obj){
-                        if (!obj.image) {
-                            str += "<li><img src='/resources/img/attach.png'>"
-                                + obj.fileName + "</li>";
-                        } else {
-                            //str += "<li>" + obj.fileName + "</li>";
-                            var fileCallPath = encodeURIComponent(obj.uploadPath+ "/s_"+ obj.uuid+"_"+obj.fileName);
-                            str += "<li><img src='/display?fileName="+fileCallPath+"'><li>";
+                        if(!obj.image){
+
+                            var fileCallPath =  encodeURIComponent( obj.uploadPath+"/"+ obj.uuid +"_"+obj.fileName);
+
+                            var fileLink = fileCallPath.replace(new RegExp(/\\/g),"/");
+
+                            str += "<li><div><a href='/download?fileName="+fileCallPath+"'>" +
+                                "<img src='/resources/img/attach.png'>"+obj.fileName+"</a>"+
+                                "<span data-file=\'"+fileCallPath+"\' data-type='file'> x </span>"+"</div></li>"
+                        }else{
+
+                            var fileCallPath =  encodeURIComponent( obj.uploadPath+ "/s_"+obj.uuid +"_"+obj.fileName);
+
+                            var originPath = obj.uploadPath+ "\\"+obj.uuid+"_"+obj.fileName;
+                            originPath = originPath.replace(new RegExp(/\\/g), "/");
+
+                            str += "<li><a href=\"javascript:showImage(\'"+originPath+"\')\">" +
+                                "<img src='/display?fileName="+fileCallPath+"'></a>" +
+                                "<span data-file=\'"+fileCallPath+"\' data-type='image'> x </span>"+"<li>";
                         }
 
                     });
